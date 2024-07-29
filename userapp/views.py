9feb75426271e0009f6f50792f2908652d5ce9b1
Enes Gemci <yenesgmc@gmail.com>
2024-07-29 19:51:16 +0300

    delete

--- userapp/views.py
+++ userapp/views.py
@@ -5,23 +5,29 @@ from .serializers import UserSerializer, PostSerializer, CommentSerializer, Albu
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
 
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    http_method_names = ['get', 'post', 'put', 'delete']
